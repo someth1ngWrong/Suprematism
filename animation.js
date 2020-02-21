181a72dc172abf43209096f98c764d9bafe34b54
Anatoly Nikiforov <tniks.work@gmail.com>
2020-02-21 06:56:15 +0300

    css fin

--- animation.js
+++ animation.js
@@ -1,1 +1,38 @@
-console.log(10);
+
+let animationBox01 = anime({
+  targets: '.box-01',
+  backgroundColor: '#ffffff',
+  left: 30,
+  easing: 'easeInOutQuad',
+  duration: 300,
+  autoplay: false
+})
+
+// let animationBox02 = anime({
+//   targets: '.box-02',
+//   backgroundColor: '#000000',
+//   easing: 'easeInOutQuad',
+//   duration: 300,
+//   autoplay: false,
+// })
+
+
+
+let htmlBox01 = document.getElementById('htmlBox01')
+htmlBox01.onclick = animationBox01.play
+
+
+
+// let animationBox02 = anime({
+//   targets: '.box-02',
+//   backgroundColor: '#000000',
+//   easing: 'easeInOutQuad',
+//   duration: 300,
+//   autoplay: false,
+// })
+
+// let htmlBox02 = document.getElementById('htmlBox02')
+// htmlBox02.onclick = animationBox02.play
+
+
+// document.querySelector('.play-pause-demo .play')
